80b5fbd2a972d68f8b6600db4693b9d88f37b1af
felipemoreira-data <felipemoreira.data@gmail.com>
2026-04-08 13:27:37 -0300

    adaptando a conexão do db para o streamlit secrets

--- db_connect.py
+++ db_connect.py
@@ -1,15 +1,25 @@
 import os
 import psycopg2 
+import streamlit as st
 from dotenv import load_dotenv
 
-load_dotenv()
 
 def conectar():
-    conn = psycopg2.connect(
-        host = os.getenv("DB_HOST"),
-        database = os.getenv("DB_NAME"),
-        user = os.getenv("DB_USER"),
-        password = os.getenv("DB_PASSWORD"),
-        port = os.getenv("DB_PORT")
-    )
+    if "DB_HOST" in st.secrets:
+        conn = psycopg2.connect(
+            host=st.secrets["DB_HOST"],
+            database=st.secrets["DB_NAME"],
+            user=st.secrets["DB_USER"],
+            password=st.secrets["DB_PASSWORD"],
+            port=st.secrets["DB_PORT"]
+        )
+    else: 
+        load_dotenv()
+        conn = psycopg2.connect(
+            host = os.getenv("DB_HOST"),
+            database = os.getenv("DB_NAME"),
+            user = os.getenv("DB_USER"),
+            password = os.getenv("DB_PASSWORD"),
+            port = os.getenv("DB_PORT")
+        )
     return conn
